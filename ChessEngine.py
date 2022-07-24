e4a3fc106c7d9224c91a79ca04771dde46df3def
Sirpip91 <allenbradley91@hotmail.com>
2022-07-24 12:31:55 -0500

    bugs

--- ChessEngine.py
+++ ChessEngine.py
@@ -6,7 +6,7 @@ class GameState():
   def __init__(self):
     #Board 8x8 2d list, 2 char each element, b or w then peice type Q,K,B ect..
     #two dash "--" means empty space.
-    self.Board =[
+    self.board =[
       ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
       ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
       ["--", "--", "--", "--", "--", "--", "--", "--"],
--- Main.py
+++ Main.py
@@ -2,8 +2,11 @@
 Python Main driver file. Handles the user input and shows gamestate obj information.
 """
 
+from time import clock_getres
 import pygame as p
+from torch import true_divide
 import ChessEngine
+import os
 
 #define spacing
 WIDTH = HEIGHT = 512
@@ -18,7 +21,7 @@ def loadImages():
   #IMAGES ['wP'] = p.image.load("img/wP.png")
   pieces = ['wP', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bP','bR','bN','bB','bK','bQ']
   for piece in pieces:
-    IMAGES[piece] = p.transform.scale(p.image.load("img/" + piece + ".png"), (SQ_SIZE,SQ_SIZE))
+    IMAGES[piece] = p.transform.scale(p.image.load("/img" + piece + ".png"), (SQ_SIZE,SQ_SIZE))
 
 
 
@@ -26,9 +29,37 @@ def loadImages():
 
 def main():
     p.init()
+    
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    print(gs)
-  
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            clock.tick(MAX_FPS)
+            p.display.flip()
+            drawGameState(screen,gs)
+
+
+#draws all graphics
+def drawGameState(screen,gs):
+  drawBoard(screen) #squares
+  drawPieces(screen,gs.board) #pieces
+
+#top left is always white
+def drawBoard(screen):
+  colors = [p.Color("white"), p.Color("gray")]
+  for r in range(DIMENSION):
+    for c in range(DIMENSION):
+         color = colors[((r+c)%2)]
+         p.draw.rect(screen,color,p.Rect(c*SQ_SIZE, r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+def drawPieces(screen,board):
+  pass
+
+if __name__ == "__main__":
+ main()
